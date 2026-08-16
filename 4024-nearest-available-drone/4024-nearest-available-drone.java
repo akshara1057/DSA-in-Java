dc527cb5321cea1d3c039399c3ae9b19aa615c51
Akshara Goyal <aksharagoyal18@gmail.com>
2026-08-16 18:37:45 +0530

    Time: 1 ms (100%), Space: 46.3 MB (81.59%) - LeetHub

--- 4024-nearest-available-drone/4024-nearest-available-drone.java
+++ 4024-nearest-available-drone/4024-nearest-available-drone.java
@@ -1,15 +1,12 @@
 class Solution {
-    public int nearestDrone(int[][] drones, int[] target) {
-        int r = drones.length;
-        int c = 3;
-        int x = target[0];
-        int y = target[1];
+    public int nearestDrone(int[][] drones, int[] t) {
         int min = Integer.MAX_VALUE;
         int index = -1;
-        for (int i = 0; i < r; i++) {
+        
+        for (int i = 0; i < drones.length; i++) {
             int a = drones[i][0];
             int b = drones[i][1];
-            int dist = Math.abs(a - x) + Math.abs(b - y);
+            int dist = Math.abs(a - t[0]) + Math.abs(b - t[1]);
             if (dist <= drones[i][2] && dist < min) {
                 index = i;
                 min = dist;
