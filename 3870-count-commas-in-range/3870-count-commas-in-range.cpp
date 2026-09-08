78248b89b96a2c1a962a71b01ffb2cfa4706ed56
Akshara Goyal <aksharagoyal18@gmail.com>
2026-09-08 23:00:50 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int temp=n;
-         int digit=0;
-        while(n>0){
-            digit++;
-            n=n/10;
-        }
-        if(digit<=3){
+        if(n<1000){
             return 0;
         }
-        int ans = temp-1000;
-        return ans+1;
+        return n-1000+1;
     }
 };
