7506cb040ff3bb0fa40297496c7116a0233fc9c1
Akshara Goyal <aksharagoyal18@gmail.com>
2026-09-24 13:56:18 +0530

    Time: 2 ms (99.3%), Space: 44 MB (95.68%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.toLowerCase();
-        char arr[] = s.toCharArray();
-        int i=0;
-        int j=arr.length-1;
-        while(i<j){
-            if(!Character.isLetterOrDigit(arr[i])){
+        int i = 0;
+        int j = s.length() - 1;
+        
+        while (i < j) {
+            // Check original string directly without using extra array memory
+            if (!Character.isLetterOrDigit(s.charAt(i))) {
                 i++;
             }
-            else if(!Character.isLetterOrDigit(arr[j])){
+            else if (!Character.isLetterOrDigit(s.charAt(j))) {
                 j--;
             }
-            else{
-                if(arr[i]!=(arr[j])){
+            else {
+                // Lowercase conversion happens on-the-fly inside the comparison
+                if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
                     return false;
                 }
                 i++;
